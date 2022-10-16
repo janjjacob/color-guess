509eedbc4988ca225d624787917b8d5024a76dfe
janjjacob <janjjacob@gmail.com>
2022-10-15 23:51:39 -0400

    Updates

---
+++ precache-manifest.c358b69986baa376878ea7775088fe52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc8a111b71052abd5223",
+    "url": "/static/css/main.056c6c75.chunk.css"
+  },
+  {
+    "revision": "dc8a111b71052abd5223",
+    "url": "/static/js/main.dc8a111b.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "2775aabee19c06da52a5b9f010346beb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/color-guessing-game/precache-manifest.5399b36eb9c8858af339e2c3ab75b997.js"
+  "/precache-manifest.c358b69986baa376878ea7775088fe52.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/color-guessing-game/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
